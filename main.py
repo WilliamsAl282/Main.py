40d5bb08c1f2885e66d89b6f96c5b03d34418a27
WilliamsAl282 <WilliamsAl282@northwested.org>
2024-09-09 11:02:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 # NOTE: Add your first and last name and the current date to your comment block
 
-# Mike Jenkins
-# 06 SEP 2024
+# Alex Williams
+# 09 SEP 2024
 # Printing in Python
+# Exercise 2 - 3  (Personal Message)
+
+message = "Hello Doug, how was ctc today?"
+print(message)
+
+#  Exercise 2 - 5 (Famous Quote)
+
+[write your code here]
+
+# Exercise 2 - 6 (Famous Quote 2)
+
+[write your code here]

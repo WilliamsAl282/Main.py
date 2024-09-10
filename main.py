1edefaee4786acd00801600b2917cfa41559f463
WilliamsAl282 <WilliamsAl282@northwested.org>
2024-09-10 10:53:07 -0400

    Update main.py
    new things

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ last_name = 'williams'
 name = 'alex williams'
 print(name.title())
 
+
+message = 'Captain America said, "I can do this all day"' 
 # Exercise 2 - 6 (Famous Quote 2)
-
-
+famous_name = 'sun tzu'
+message = (f' {famous_name} once said, If you know the enemy and know yourself, you need not fear the result of a hundred battles. ')
+print(message)
